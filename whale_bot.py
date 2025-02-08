28c87393a0d7ca2baa2a1148b4f82ecfe1ba94aa
SheykhDevBr <alirezabavaghar2004@gmail.com>
2025-02-08 21:49:53 +0330

    Update whale_bot.py

--- whale_bot.py
+++ whale_bot.py
@@ -60,4 +60,4 @@ def track_token(message):
 if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=WEBHOOK_URL)
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)), threaded=True)
